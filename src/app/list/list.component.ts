ff12dcf6954327ff40fc9c9278bcd6a94a5dab55
Daniel Antonio Nolasco Alvarado <dnieln7@gmail.com>
2019-10-23 21:15:39 -0500

    add google maps api

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {} from 'googlemaps';
+import Marker = google.maps.Marker;
+import LatLng = google.maps.LatLng;
+import Map = google.maps.Map;
 
 @Component({
   selector: 'app-list',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  @ViewChild('map', {static: true}) mapElement: any;
+  map: google.maps.Map;
+
   constructor() { }
 
   ngOnInit() {
+    const myPosition = new LatLng(35.2271, -80.8431);
+
+    const mapProperties = {
+      center: new google.maps.LatLng(35.2271, -80.8431),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+    };
+
+    const myMap = new Map(this.mapElement.nativeElement, {
+      zoom: 10,
+      center: myPosition
+    });
+
+    const marker = new Marker({
+      position: myPosition,
+      map: myMap,
+      title: 'hi!'
+    });
+
+    this.map = myMap;
   }
 
 }
