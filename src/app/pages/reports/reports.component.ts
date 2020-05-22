1054b471c8960b45ed6af70a0be70bdd035477ed
Daniel Antonio Nolasco Alvarado <dnieln7@gmail.com>
2020-05-22 13:31:06 -0500

    migrated to bootstrap

---
+++ src/app/pages/reports/reports.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {Report} from '../../models/Report';
+import {ReportDataService} from '../../services/report.data.service';
+
+@Component({
+  selector: 'app-reports',
+  templateUrl: './reports.component.html',
+  styleUrls: ['./reports.component.scss']
+})
+export class ReportsComponent implements OnInit {
+
+  reports: Array<Report>;
+
+  constructor(private dataService: ReportDataService) {
+  }
+
+  ngOnInit(): void {
+    this.reports = [];
+
+    this.getReports();
+  }
+
+  getReports() {
+    this.dataService.findAll(1).subscribe(value => {
+      this.reports = value;
+    });
+  }
+
+  log(report) {
+    console.log(report);
+  }
+}
